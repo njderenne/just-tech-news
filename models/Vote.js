770b4f49ae5eca480e90bd2cfcf0c6ea776ad828
In Code We Trust <njderenne@gmail.com>
2020-11-28 09:59:17 -0600

    created vote file

---
+++ models/Vote.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model {}
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
